e34aeca6246d253f59f6250fcd7c57065c3b39e2
Stuart Leeks <stuartle@microsoft.com>
2024-10-17 08:53:13 +0000

    Add simple metric for retry count

--- subscriber-app/metrics.py
+++ subscriber-app/metrics.py
@@ -9,9 +9,19 @@ _histogram_open_ai_embeddings_requests = meter.create_histogram(
     unit="requests",
 )
 
+_histogram_open_ai_embeddings_retries = meter.create_histogram(
+    name="subscriber-app.openai.embeddings.retries",
+    description="Number of OpenAI embeddings requests that got a 429 retry response",
+    unit="requests",
+)
+
 
 def increment_open_ai_retry(status: int):
     _histogram_open_ai_embeddings_requests.record(
         1,
         {"status": str(status)}
     )
+    if status == 429:
+        # TODO - consider updating the external scaler to enable using the status code on the
+        # requests metric rather than adding a new metric
+        _histogram_open_ai_embeddings_retries.record(1)
